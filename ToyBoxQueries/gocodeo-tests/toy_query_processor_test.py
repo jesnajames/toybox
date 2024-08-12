e9eedaaca74687f49f044bc65eb84062d40b39ed
Jesna James <16302812+jesnajames@users.noreply.github.com>
2024-08-13 05:27:51 +0530

    GoCodeo ADD tests

--- ToyBoxQueries/gocodeo-tests/toy_query_processor_test.py
+++ ToyBoxQueries/gocodeo-tests/toy_query_processor_test.py
@@ -1,1 +1,17 @@
-This is a sample file
+```json
+{
+    "happy path": [
+        {
+            "behaviour on which test case will be generated using below data": {
+                "name of function from input source code on which test cases is being generated": "def test_get_toy_happy_path(toy_query_processor):\n    toy_data = {'toy_id': '123', 'name': 'Toy Car', 'price': 10.99}\n    DBConnector.get_item.return_value = toy_data\n    toy = toy_query_processor.get_toy('123')\n    assert toy['toy_id'] == '123'\n    assert toy['name'] == 'Toy Car'\n    assert toy['price'] == 10.99"
+            }
+        }
+    ],
+    "edge case": [
+        {
+            "behaviour on which test case will be generated using below data": {
+                "name of function from input source code on which test cases is being generated": "def test_get_toy_not_found(toy_query_processor):\n    DBConnector.get_item.return_value = None\n    with pytest.raises(NotFoundException) as excinfo:\n        toy_query_processor.get_toy('999')\n    assert str(excinfo.value) == 'Toy 999 not found'"
+            }
+        }
+    ]
+}
