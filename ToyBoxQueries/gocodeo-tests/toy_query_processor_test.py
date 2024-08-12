2b2a6ff734c0dbfb6d5abbbe536092b7e6c2c5b4
Jesna James <16302812+jesnajames@users.noreply.github.com>
2024-08-13 05:19:14 +0530

    GoCodeo ADD tests

--- ToyBoxQueries/gocodeo-tests/toy_query_processor_test.py
+++ ToyBoxQueries/gocodeo-tests/toy_query_processor_test.py
@@ -1,1 +1,17 @@
-This is a sample file
+```json
+{
+    "happy path": [
+        {
+            "behaviour  on which test case will be generated using below data": {
+                "name of function from input source code on which test cases is being generated": "def test_get_toy_happy_path(mock_db_connector, toy_query_processor):\n    toy_data = {'toy_id': '123', 'name': 'ToyName', 'price': 19.99}\n    mock_db_connector.get_item.return_value = toy_data\n    toy = toy_query_processor.get_toy('123')\n    assert toy.toy_id == '123'\n    assert toy.name == 'ToyName'\n    assert toy.price == 19.99"
+            }
+        }
+    ],
+    "edge case": [
+        {
+            "behaviour  on which test case will be generated using below data": {
+                "name of function from input source code on which  this test plan, it shouldn't be in a key :value json formart": "def test_get_toy_not_found(mock_db_connector, toy_query_processor):\n    mock_db_connector.get_item.return_value = None\n    with pytest.raises(NotFoundException) as exc_info:\n        toy_query_processor.get_toy('999')\n    assert str(exc_info.value) == 'Toy 999 not found'"
+            }
+        }
+    ]
+}
