9d269b97fd35c9e1e14523e15541d60771045b0c
Jesna James <16302812+jesnajames@users.noreply.github.com>
2024-08-13 05:16:00 +0530

    GoCodeo ADD tests

--- ToyBoxQueries/gocodeo-tests/toy_query_processor_test.py
+++ ToyBoxQueries/gocodeo-tests/toy_query_processor_test.py
@@ -1,1 +1,17 @@
-This is a sample file
+```json
+{
+    "happy path": [
+        {
+            "behaviour on which test case will be generated using below data": {
+                "name of function from input source code on which test cases is being generated": "def test_get_toy_happy_path(mock_db_connector, toy_query_processor):\n    toy_id = \"123\"\n    toy_data = {\"toy_id\": toy_id, \"name\": \"Toy Name\", \"type\": \"Toy Type\"}\n    mock_db_connector.get_item.return_value = toy_data\n    toy = toy_query_processor.get_toy(toy_id)\n    assert toy.toy_id == toy_id\n    assert toy.name == \"Toy Name\"\n    assert toy.type == \"Toy Type\""
+            }
+        }
+    ],
+    "edge case": [
+        {
+            "behaviour on which test case will be generated using below data": {
+                "name of function from input source code on which this test plan, it shouldn't be in a key :value json formart": "def test_get_toy_not_found(mock_db_connector, toy_query_processor):\n    toy_id = \"123\"\n    mock_db_connector.get_item.return_value = None\n    with pytest.raises(NotFoundException) as exc_info:\n        toy_query_processor.get_toy(toy_id)\n    assert str(exc_info.value) == f\"Toy {toy_id} not found\""
+            }
+        }
+    ]
+}
